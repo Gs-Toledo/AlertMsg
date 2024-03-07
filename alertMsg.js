e854f9476e3e165acf7f7a3bf300016949cdf294
Giovanni Toledo <giovanni.toledo@injunior.com.br>
2024-03-07 09:55:18 -0300

    chore: new script dist

--- alertMsg.js
+++ alertMsg.js
@@ -1,51 +1,40 @@
 import './styles/styles.css';
-
 class FixedAlert {
-    private message: string;
-    private alertType: string;
-    private container: HTMLElement;
-    private alertDiv?: HTMLDivElement;
-
-    constructor(message: string, alertType: string, container: HTMLElement = document.body) {
-
+    message;
+    alertType;
+    container;
+    alertDiv;
+    constructor(message, alertType, container = document.body) {
         this.message = message;
         this.alertType = alertType;
         this.container = container;
         this.createAlert();
         this.autoRemove();
-
     }
-
-    private createAlert(): void {
+    createAlert() {
         const alertDiv = document.createElement('div');
         alertDiv.className = `alert alert-${this.alertType} fixed-top-alert fade show`;
         alertDiv.textContent = this.message;
-
         alertDiv.addEventListener('transitionend', () => {
             if (!alertDiv.classList.contains('show')) {
                 this.container.removeChild(alertDiv);
             }
         });
-
         this.container.appendChild(alertDiv);
-
         setTimeout(() => {
             alertDiv.classList.remove('show');
         }, 3000);
-
         this.alertDiv = alertDiv;
-
     }
-
-    private autoRemove(): void {
+    autoRemove() {
         setTimeout(() => {
             this.removeAlert();
         }, 4000);
     }
-
-    private removeAlert(): void {
+    removeAlert() {
         if (this.alertDiv) {
             this.alertDiv.classList.add('show');
         }
     }
 }
+export default FixedAlert;
